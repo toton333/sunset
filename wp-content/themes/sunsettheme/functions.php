14d0019ade676353d922ec9749aed10c55145f97
toton333 <toton333@yahoo.com>
2017-10-27 23:02:37 +0530

    added header

--- wp-content/themes/sunsettheme/functions.php
+++ wp-content/themes/sunsettheme/functions.php
@@ -30,6 +30,32 @@ add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts' );
 
 /*
 	
+	========================
+		FRONT-END ENQUEUE FUNCTIONS
+	========================
+*/
+function sunset_load_scripts(){
+	
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.7', 'all' );
+	wp_enqueue_style( 'sunset', get_template_directory_uri() . '/css/sunset.frontend.css', array(), '1.0.0', 'all' );
+	wp_enqueue_style( 'raleway', 'https://fonts.googleapis.com/css?family=Raleway:200,300,500' );
+	
+
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
+    wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
+	
+}
+add_action( 'wp_enqueue_scripts', 'sunset_load_scripts' );
+
+
+
+
+
+
+/*
+	
 @package sunsettheme
 	
 	========================
